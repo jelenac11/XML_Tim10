134fd44f28b6225a0a9d85a563c6c6ff898ad1a8
miki-lauda <52676856+miki-lauda@users.noreply.github.com>
2020-12-01 17:19:08 +0100

    Created initial spring boot project and projected schemas for request and notification

---
+++ team9/src/main/java/projectXML/team9/Team9Application.java
@@ -0,0 +1,13 @@
+package projectXML.team9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team9Application.class, args);
+	}
+
+}
---
+++ team9/src/test/java/projectXML/team9/Team9ApplicationTests.java
@@ -0,0 +1,13 @@
+package projectXML.team9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
