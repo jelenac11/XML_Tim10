500539812fd1ce7552d68376c17852423c9db0d0
miki-lauda <52676856+miki-lauda@users.noreply.github.com>
2021-02-06 10:52:57 +0100

    Implemented creating collections when app is started

--- apps/poverenik/poverenik/src/main/java/projectXML/team10/poverenik/PoverenikApplication.java
+++ apps/poverenik/poverenik/src/main/java/projectXML/team10/poverenik/PoverenikApplication.java
@@ -3,9 +3,11 @@ package projectXML.team10.poverenik;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.xmldb.api.base.XMLDBException;
 
 import projectXML.team10.poverenik.models.korisnik.Korisnik;
 import projectXML.team10.poverenik.services.KorisnikService;
+import projectXML.team10.poverenik.util.DatabaseConnector;
 
 @SpringBootApplication
 public class PoverenikApplication {
@@ -13,7 +15,20 @@ public class PoverenikApplication {
 	public static void main(String[] args) {
 		System.setProperty("javax.xml.bind.JAXBContext", "com.sun.xml.internal.bind.v2.ContextFactory");
 		ConfigurableApplicationContext appContext = SpringApplication.run(PoverenikApplication.class, args);
-	    KorisnikService service = appContext.getBean(KorisnikService.class);
+	    
+		DatabaseConnector connector = appContext.getBean(DatabaseConnector.class);
+		try {
+			connector.getOrCreateCollection("/db/sample/izvestaji", 0);
+			connector.getOrCreateCollection("/db/sample/korisnici", 0);
+			connector.getOrCreateCollection("/db/sample/odlukePoverioca", 0);
+			connector.getOrCreateCollection("/db/sample/zalbeCutanje", 0);
+			connector.getOrCreateCollection("/db/sample/zalbeNaOdluku", 0);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | XMLDBException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		KorisnikService service = appContext.getBean(KorisnikService.class);
 	    Korisnik k = new Korisnik("Milan", "Marinović", "poverenik@gmail.com", "sifra123", "poverenik", "");
 	    try {
 	    	if (service.findByEmail("poverenik@gmail.com") == null)
--- apps/sluzbenik/team9/src/main/java/projectXML/team9/Team9Application.java
+++ apps/sluzbenik/team9/src/main/java/projectXML/team9/Team9Application.java
@@ -3,17 +3,31 @@ package projectXML.team9;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.xmldb.api.base.XMLDBException;
 
 import projectXML.team9.models.korisnik.Korisnik;
 import projectXML.team9.services.KorisnikService;
+import projectXML.team9.util.DatabaseConnector;
 
 @SpringBootApplication
 public class Team9Application {
 
 	public static void main(String[] args) {
-		
+
 		ConfigurableApplicationContext appContext = SpringApplication.run(Team9Application.class, args);
 		KorisnikService service = appContext.getBean(KorisnikService.class);
+
+		DatabaseConnector connector = appContext.getBean(DatabaseConnector.class);
+		try {
+			connector.getOrCreateCollection("/db/sample/zahtevi", 0);
+			connector.getOrCreateCollection("/db/sample/korisnici", 0);
+			connector.getOrCreateCollection("/db/sample/obavestenja", 0);
+			connector.getOrCreateCollection("/db/sample/izvestaji", 0);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | XMLDBException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		Korisnik k = new Korisnik("Marko", "Marković", "noreply.kts.l9@gmail.com", "sifra123", "sluzbenik", "");
 		try {
 			if (service.findByEmail("noreply.kts.l9@gmail.com") == null)
